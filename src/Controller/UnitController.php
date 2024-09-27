e74029c183a3cca0ca9f36d40a92508b186713a0
KHELIFI Walid <khelifiwaleed@gmail.com>
2024-09-27 23:06:20 +0100

    prepar work environment

---
+++ src/Controller/UnitController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use CacheLoader\CacheLoaderBundle\CacheLoaderBundle;
+
+class UnitController extends AbstractController
+{
+
+    public function __invoke(
+        CacheLoaderBundle $cacheLoaderBundle
+    ): Response
+    {
+        return $this->render("base.html.twig");
+    }
+}
